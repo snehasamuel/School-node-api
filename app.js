5a69c990aa2564cffd0470a015db16f930ca0644
snehasamuel <snehasamuel297@gmail.com>
2022-05-25 21:57:41 +0530

    api pending

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ let app=Express()
 app.use(Bodyparser.urlencoded({extended:true}))
 app.use(Bodyparser.json())
 
+var studentModel=Mongoose.model("students",
+new Mongoose.Schema(
+    {
+        Admissionno:String,
+        Rollno:String,
+        Name:String,
+        Class:String,
+        Parent:String,
+        Phone:String,
+        Address:String
+    }
+))
+
+Mongoose.connect("mongodb+srv://snehasam:snehasa4@cluster0.yyrcr.mongodb.net/Collegedb")
 
 app.post("/api/addstudent",(req,res)=>{
 var getAdmnno=req.body.admnno 
@@ -16,12 +30,36 @@ var getClass=req.body.classes
 var getParent=req.body.parent 
 var getPhn=req.body.phone 
 var getAddress=req.body.address 
-var data={"Admissionno":getAdmnno,"Rollno":getRollno,"Name":getName,"Class":getClass,"Parent":getParent,"Phone":getPhn,"Address":getAddress}
-res.send(data)
+var datas={"Admissionno":getAdmnno,"Rollno":getRollno,"Name":getName,"Class":getClass,"Parent":getParent,"Phone":getPhn,"Address":getAddress}
+let studentadd=new studentModel(datas)
+studentadd.save((error,datas)=>{
+    if(error)
+    {
+        res.send({"status":"error","datas":error})
+    }
+    else{
+        res.send({"status":"success","datas":datas})
+    }
+})
+})
+
+app.post("/api/addfaculty",(req,res)=>{
+    var getName=req.body.name 
+    var getMobile=req.body.mobile 
+    var getEducation=req.body.education 
+    var getAddress=req.body.address 
+    var getPin=req.body.pincode
+    var getDistrict=req.body.district 
+    var details={"Name":getName,"Mobile":getMobile,"Education":getEducation,"Address":getAddress,"Pincode":getPin,"District":getDistrict}
+    res.send(details)
 })
 
 app.get("/api/viewstudent",(req,res)=>{
     res.send("Hello")
+})
+
+app.get("/api/viewfaculty",(req,res)=>{
+    res.send("Welcome")
 })
 
 app.listen(5000,()=>{
